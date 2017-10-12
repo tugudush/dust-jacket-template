431c89cf677e63a287c5d26b72819ccba1f0427a
tugudush <jerome2kph@gmail.com>
2017-10-12 19:41:20 +0800

    changed index.php title and description

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once('functions.inc.php');
 <html lang="en">
 <head>
     <?php require_once('meta.inc.php'); ?>
-    <meta name="description" content="Page Description">
-    <title>Page Title</title>    
+    <meta name="description" content="Dust Jacket">
+    <title>Dust Jacket</title>    
     <?php require_once('styles.inc.php'); ?>
     <?php include_once('favicons.inc.php'); ?>
     <?php include_once('google-fonts.inc.php'); ?>
@@ -17,7 +17,7 @@ require_once('functions.inc.php');
     <?php include_once('header.inc.php'); ?>
     <div id="container">
         <div class="wrap">
-            
+            test
         </div><!--/.wrap-->
     </div><!--/container-->
     <?php include_once('footer.inc.php'); ?>
