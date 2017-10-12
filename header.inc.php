115e41df6ff45ab485e582d5915a5e1bce2d6d9e
tugudush <jerome2kph@gmail.com>
2017-10-13 05:28:54 +0800

    header/banner

---
+++ header.inc.php
@@ -0,0 +1,42 @@
+<div id="banner">
+  <div class="wrap">
+    <div id="top-bar" class="clearfix">
+
+      <div class="social-icons">
+        <a href="#"><span class="fa fa-facebook"></span></a>
+        <a href="#"><span class="fa fa-twitter"></span></a>
+        <a href="#"><span class="fa fa-google-plus"></span></a>
+        <a href="#"><span class="fa fa-instagram"></span></a>
+      </div><!--/.social-icons-->
+
+      <div class="quick-links">
+        <a href="#">Login</a>
+        <a href="#">Sign Up</a>
+        <a href="#">Cart</a>
+      </div><!--/.quick-links-->          
+    </div><!--/top-bar-->
+
+    <img id="logo" src="images/logo.jpg">
+
+      <div id="main-nav-wrap">
+        <ul id="main-nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div><!--/main-nav-wrap-->
+
+      <div id="search-wrap">
+        <form id="search-form" action="" method="post" class="clearfix">
+          <input type="text" id="search-keyword" name="search_keyword">
+          <input type="submit" id="search-submit" name="search_submit" value="SEARCH">
+        </form><!--/search-form-->
+      </div><!--/search-wrap-->
+
+      <p class="text1">
+        <strong>IS A BOOK NOT COMING UP ON A SEARCH OF OUR WEBSITE?</strong><br>
+        <span class="text1b">Send an Enquiry</span> through to us now so we can order the book you need!
+      </p>
+
+  </div><!--/.wrap-->
+</div><!--/banner-->
--- index.php
+++ index.php
@@ -16,26 +16,7 @@ require_once('functions.inc.php');
 <body id="page-home">        
   <?php include_once('header.inc.php'); ?>
   <div id="container">
-    <div id="banner">
-      <div class="wrap">
-        <div id="top-bar" class="clearfix">
-          
-          <div class="social-icons">
-            <a href="#">f</a>
-            <a href="#">t</a>
-            <a href="#">g</a>
-            <a href="#">i</a>
-          </div><!--/.social-icons-->
-          
-          <div class="quick-links">
-            <a href="#">Login</a>
-            <a href="#">Sign Up</a>
-            <a href="#">Cart</a>
-          </div><!--/.quick-links-->
-          
-        </div><!--/top-bar-->
-      </div><!--/.wrap-->
-    </div><!--/banner-->
+    
   </div><!--/container-->
   <?php include_once('footer.inc.php'); ?>
   <?php require_once('footer-scripts.inc.php'); ?>    
